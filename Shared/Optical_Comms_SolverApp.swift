ff03f4caf7f859a9e3b7feb3fc82beee614b66a7
Juan Diego Ocampo <gummy_pond0v@icloud.com>
2021-10-15 20:34:00 -0500

    Initial Commit

---
+++ Shared/Optical_Comms_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Optical_Comms_SolverApp.swift
+//  Shared
+//
+//  Created by Juan Diego Ocampo on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Optical_Comms_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
