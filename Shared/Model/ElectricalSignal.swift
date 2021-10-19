a9cfea860053b5fec1fbf1d06057861da0e32d82
Juan Diego Ocampo <gummy_pond0v@icloud.com>
2021-10-19 17:51:50 -0500

    Code reformatting for optional unwrapping and nil error handling

--- Shared/Model/ElectricalSignal.swift
+++ Shared/Model/ElectricalSignal.swift
@@ -9,11 +9,13 @@ import Foundation
 
 struct ElectricalSignal: Identifiable {
     
+    let speedOfLight: Double = 3E8
     var id = UUID()
     var frequency: String?
     var bandwidth: String?
     var title: String = "Frequency to Wavelength"
-
+    var wavelength: String { return toOpticalDomain(frequency: frequency, bandwidth: bandwidth).wavelength }
+    var spectralWidth: String { return toOpticalDomain(frequency: frequency, bandwidth: bandwidth).spectralWidth }
     var lowerLimit: String {
         if let frequency = Double(frequency!), let bandwidth = Double(bandwidth!) {
             let formatter = NumberFormatter()
@@ -25,7 +27,6 @@ struct ElectricalSignal: Identifiable {
             return "-"
         }
     }
-    
     var upperLimit: String {
         if let frequency = Double(frequency!), let bandwidth = Double(bandwidth!) {
             let formatter = NumberFormatter()
@@ -37,42 +38,37 @@ struct ElectricalSignal: Identifiable {
             return "-"
         }
     }
-    
-    var wavelength: String {
-        return frequencyToWavelength(frequency: frequency, bandwidth: bandwidth).wavelength
+    var formatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 3
+        return formatter
     }
     
-    var spectralWidth: String {
-        return frequencyToWavelength(frequency: frequency, bandwidth: bandwidth).spectralWidth
-    }
-    
-    func frequencyToWavelength(frequency: String?, bandwidth: String?) -> (wavelength: String, spectralWidth: String) {
-        if let unrappedFrequency = Double(frequency!), let unrapppedBandwidth = Double(bandwidth!) {
-            let frequency = unrappedFrequency * 1E12
-            _ = unrapppedBandwidth * 1E12
-            let lowerLimit = Double(self.lowerLimit)! * 1E12
-            let upperLimit = Double(self.upperLimit)! * 1E12
-            let formatter = NumberFormatter()
-            formatter.numberStyle = .decimal
-            formatter.maximumFractionDigits = 1
-            formatter.usesGroupingSeparator = false
-            /// Speed of Light Constant
-            let speedOfLight: Double = 3E8
-            /// Compute Frequency from Wavelength
-            let lambda: Double = (speedOfLight / frequency) * 1E9
-            let wavelength = formatter.string(from: NSNumber(value: lambda))!
-            /// Compute Spectral Width
-            let spectralWidthLowerLimit: Double = (speedOfLight / lowerLimit)
-            let spectralWidthUpperLimit: Double = (speedOfLight / upperLimit)
-            let deltaLambda: Double = (spectralWidthLowerLimit - spectralWidthUpperLimit) * 1E9
-            let spectralWidth = formatter.string(from: NSNumber(value: deltaLambda))!
-            /// Display Results in THz
-            return (wavelength, spectralWidth)
-        } else {
-            let wavelength = "-"
-            let spectralWidth = "-"
+    /// Tag: Convert from Electrical to Optical Domain
+    func toOpticalDomain(frequency: String?, bandwidth: String?) -> (wavelength: String, spectralWidth: String) {
+        // Optional Unwrapping
+        guard let frequency = frequency, let bandwidth = bandwidth else {
+            let wavelength = "-", spectralWidth = "-"
             return (wavelength, spectralWidth)
         }
+        guard let frequency = Double(frequency) , let bandwidth = Double(bandwidth) else {
+            let wavelength = "-", spectralWidth = "-"
+            return (wavelength, spectralWidth)
+        }
+        // Unit Scaling
+        let scaledFrequency = frequency * 1E12
+        _ = bandwidth * 1E12
+        let lowerLimit = Double(self.lowerLimit)! * 1E12
+        let upperLimit = Double(self.upperLimit)! * 1E12
+        // Performing Calculations
+        let lambda: Double = (speedOfLight / scaledFrequency) * 1E9
+        let spectralWidthLowerLimit: Double = (speedOfLight / lowerLimit)
+        let spectralWidthUpperLimit: Double = (speedOfLight / upperLimit)
+        let deltaLambda: Double = (spectralWidthLowerLimit - spectralWidthUpperLimit) * 1E9
+        // String Formatting
+        let wavelength = formatter.string(from: NSNumber(value: lambda))!
+        let spectralWidth = formatter.string(from: NSNumber(value: deltaLambda))!
+        return (wavelength, spectralWidth)
     }
-    
 }
--- Shared/Model/OpticalSignal.swift
+++ Shared/Model/OpticalSignal.swift
@@ -9,45 +9,46 @@ import Foundation
 
 struct OpticalSignal: Identifiable {
     
+    let speedOfLight: Double = 3E8
     var id = UUID()
+    var title: String = "Wavelength to Frequency"
     var wavelength: String?
     var spectralWidth: String?
-    var title: String = "Wavelength to Frequency"
-    var frequency: String { return wavelengthToFrequency(lambda: wavelength, spectralWidth: spectralWidth).frequency }
-    var lowerLimit: String { return wavelengthToFrequency(lambda: wavelength, spectralWidth: spectralWidth).lowerLimit }
-    var upperLimit: String { return wavelengthToFrequency(lambda: wavelength, spectralWidth: spectralWidth).upperLimit }
-    var bandwidth: String { return wavelengthToFrequency(lambda: wavelength, spectralWidth: spectralWidth).bandwidth }
-    
-    func wavelengthToFrequency(lambda: String?, spectralWidth: String?) -> (frequency: String, upperLimit: String, lowerLimit: String, bandwidth: String) {
-        if let unrappedWavelength = Double(lambda!), let unwrappedSpectralWidth = Double(spectralWidth!) {
-            let wavelength = unrappedWavelength * 1E-9
-            let gap = unwrappedSpectralWidth * 1E-9
-            let formatter = NumberFormatter()
-            formatter.numberStyle = .decimal
-            formatter.maximumFractionDigits = 3
-            /// Speed of Light Constant
-            let speedOfLight: Double = 3E8
-            /// Compute Frequency from Wavelength
-            let f: Double = (speedOfLight / wavelength) / 1E12
-            let frequency = formatter.string(from: NSNumber(value: f))!
-            /// Compute Upper Frequency Limit (ƛ - Δƛ/2)
-            let fh: Double = (speedOfLight / (wavelength - (gap/2))) / 1E12
-            let upperLimit = formatter.string(from: NSNumber(value: fh))!
-            /// Compute Lower Frequency Limit (ƛ + Δƛ/2)
-            let fl: Double = (speedOfLight / (wavelength + (gap/2))) / 1E12
-            let lowerLimit =  formatter.string(from: NSNumber(value: fl))!
-            /// Compute Bandwidth
-            let bw: Double =  fh - fl
-            let bandwidth = formatter.string(from: NSNumber(value: bw))!
-            /// Display Results in THz
-            return (frequency, upperLimit, lowerLimit, bandwidth)
-        } else {
-            let frequency = "-"
-            let upperLimit = "-"
-            let lowerLimit = "-"
-            let bandwidth = "-"
-            return (frequency, upperLimit, lowerLimit, bandwidth)
-        }
+    var frequency: String { return toElectricalDomain(lambda: wavelength, spectralWidth: spectralWidth).frequency }
+    var lowerLimit: String { return toElectricalDomain(lambda: wavelength, spectralWidth: spectralWidth).lowerLimit }
+    var upperLimit: String { return toElectricalDomain(lambda: wavelength, spectralWidth: spectralWidth).upperLimit }
+    var bandwidth: String { return toElectricalDomain(lambda: wavelength, spectralWidth: spectralWidth).bandwidth }
+    var formatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 3
+        return formatter
     }
     
+    /// Tag: Convert from Optical to Electrical Domain
+    func toElectricalDomain(lambda: String?, spectralWidth: String?) -> (frequency: String, upperLimit: String, lowerLimit: String, bandwidth: String) {
+        // Optional Unwrapping
+        guard let lambda = lambda, let spectralWidth = spectralWidth else {
+            let frequency = "-", upperLimit = "-", lowerLimit = "-", bandwidth = "-"
+            return (frequency, upperLimit, lowerLimit, bandwidth)
+        }
+        guard let wavelength = Double(lambda) , let spectralWidth = Double(spectralWidth) else {
+            let frequency = "-", upperLimit = "-", lowerLimit = "-", bandwidth = "-"
+            return (frequency, upperLimit, lowerLimit, bandwidth)
+        }
+        // Unit Scaling
+        let scaledWavelength = wavelength * 1E-9
+        let scaledSpectralWidth = spectralWidth * 1E-9
+        // Performing Calculations
+        let f: Double = (speedOfLight / scaledWavelength) / 1E12
+        let fh: Double = (speedOfLight / (scaledWavelength - (scaledSpectralWidth/2))) / 1E12
+        let fl: Double = (speedOfLight / (scaledWavelength + (scaledSpectralWidth/2))) / 1E12
+        let bw: Double =  fh - fl
+        // String Formatting
+        let frequency = formatter.string(from: NSNumber(value: f))!
+        let upperLimit = formatter.string(from: NSNumber(value: fh))!
+        let lowerLimit =  formatter.string(from: NSNumber(value: fl))!
+        let bandwidth = formatter.string(from: NSNumber(value: bw))!
+        return (frequency, upperLimit, lowerLimit, bandwidth)
+    }
 }
